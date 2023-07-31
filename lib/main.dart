cf952fb26ecd9b84cb37923210eb64f023a4c0ac
T1TANN <abuzargore91@gmail.com>
2023-07-31 21:09:10 +0530

    day_04 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:ft1/home_page.dart';
-
+import 'package:ft1/pages/home_page.dart';
+import 'package:ft1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 void main(){
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     
-    return const MaterialApp(
+    return  MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.yellow,
+      fontFamily: GoogleFonts.lato().fontFamily,
+       ),
       
-      home:homepage()
+      routes: {
+      "/hone":(context)=>homepage(),
+      "/":(context)=>login_page(),
+     }
     );
   
   }
